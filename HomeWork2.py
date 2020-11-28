bdcdc368d39d62cbd8240a3c9dfe36cd377e8128
volodymyr <kokolus7806@gmail.com>
2020-11-28 23:37:00 +0200

    end first task

--- HomeWork2.py
+++ HomeWork2.py
@@ -1,3 +1,7 @@
-st = 'as 23 fdfdg544'
-print(st)
-print(st)
+############ 1 ####################
+string = input('Введіть стрічку\n')
+number_list = []
+for i in string:
+    if i.isnumeric():
+        number_list.append(i)
+print(','.join(number_list))
