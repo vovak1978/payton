69a9c04f69e5e8e9d4742e839a2c313ca853e428
volodymyr <kokolus7806@gmail.com>
2020-11-29 12:39:12 +0200

    finish 3 task

--- HomeWork2.py
+++ HomeWork2.py
@@ -1,7 +1,25 @@
 ############ 1 ####################
-string = input('Введіть стрічку\n')
-number_list = []
-for i in string:
-    if i.isnumeric():
-        number_list.append(i)
-print(','.join(number_list))
+#string = input('Введіть стрічку\n')
+#number_list = []
+# for i in string:
+#     if i.isnumeric():
+#         number_list.append(i)
+# print(','.join(number_list))
+
+############# 2 ##################
+string = 'as 23 fdfdg544 34'
+
+# string_list = list(string.split(" "))
+# for i in string_list:
+#     if not i.isnumeric():
+#         string_list.remove(i)
+# print(string_list)
+
+
+############# 3 ##################
+# string = input('Введіть стрічку\n')
+# string_list = []
+# for i in string:
+#     if i not in string_list:
+#         string_list.append(i)
+#         print("'",i ,"'",'-> ', string.count(i), sep='')
